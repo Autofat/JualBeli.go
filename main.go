e47de1b0d6bd362a519cedafed107ea0d8b9046c
Autofat <borneo.wibowo@gmail.com>
2023-06-01 11:45:42 +0700

    fix UI, Add Edit Func

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ type barang struct {
 	// SubType untuk memberi nama, katagori, harga modal, dan harga jual
 	nama, kategori        string
 	hargaModal, hargaJual float64
+	transaksi             int
 }
 
 type arrBarang [NMAX - 1]barang // Array untuk menyimpan data barang yang akan di jual
@@ -15,57 +16,92 @@ type arrBarang [NMAX - 1]barang // Array untuk menyimpan data barang yang akan d
 func main() {
 	var b arrBarang
 	var n, perintah_main int
-	var target string
+	var target, A string
 
-	UI_Main()
-	fmt.Print("Silahkan pilih perintah: ")
-	fmt.Scan(&perintah_main)
-	// if perintah_main == 1 {
-	clearScreen()
-	isiBarang(&b, &n)
-	displayArr(b, n)
-	// } else if perintah_main == 2 {
-	fmt.Scan(&target)
-	cekBarang(b, n, target)
-	if cekBarang(b, n, target) > 0 {
-		fmt.Println("=====================| EDIT BARANG |=====================")
-		fmt.Println("DATA DITEMUKAN")
-		fmt.Println("=====================|  TOKO KEREN  |=====================")
-	} else {
-		fmt.Println("=====================| EDIT BARANG |=====================")
-		fmt.Println("DATA TIDAK DITEMUKAN")
-		fmt.Println("=====================|  TOKO KEREN  |=====================")
+	for {
+		clearScreen()
+		UI_Main()
+		fmt.Print("Silahkan pilih perintah: ")
+		fmt.Scan(&perintah_main)
+
+		if perintah_main == 1 {
+			clearScreen()
+			isiBarang(&b, &n)
+			fmt.Println("==========================================================")
+			fmt.Print("          TEKAN [X] UNTUK KEMBALI KE HALAMAN UTAMA          ")
+			fmt.Scan(&A)
+		} else if perintah_main == 2 {
+			clearScreen()
+			fmt.Println("=====================| EDIT BARANG |======================")
+			fmt.Print("NAMA BARANG YANG INGIN DI EDIT: ")
+			fmt.Scan(&target)
+			cekBarang(b, n, target)
+			if cekBarang(b, n, target) >= 0 {
+				clearScreen()
+				fmt.Println("=====================| EDIT BARANG |======================")
+				fmt.Println("|                                                        |")
+				fmt.Println("|                   [ DATA DITEMUKAN ]                   |")
+				fmt.Println("|         SILAHKAN MASUKAN DATA YANG MAU DI EDIT         |")
+				fmt.Println("|                                                        |")
+				fmt.Println("=====================|  TOKO KEREN  |=====================")
+				fmt.Println(b[cekBarang(b, n, target)])
+				fmt.Println("==========================================================")
+				editData(&b, n, cekBarang(b, n, target))
+
+			} else {
+				clearScreen()
+				fmt.Println("=====================| EDIT BARANG |======================")
+				fmt.Println("|                                                        |")
+				fmt.Println("|                [ DATA TIDAK DITEMUKAN ]                |")
+				fmt.Println("|        SILAHKAN CEK KEMBALI DATA YANG INGIN DI EDIT    |")
+				fmt.Println("|                                                        |")
+				fmt.Println("=====================|  TOKO KEREN  |=====================")
+				fmt.Print("          TEKAN [X] UNTUK KEMBALI KE HALAMAN UTAMA          ")
+				fmt.Scan(&A)
+			}
+		}
+
 	}
-	displayArr(b, n)
-	// }
-
 }
 
 func UI_Main() {
 	// tampilan layar utama (home page awal)
-	fmt.Println("=====================|ADMIN MENU V.1|=====================")
-	fmt.Println("%s\n", "INPUT BARANG  [1]")
-	fmt.Println("%s\n", "EDIT BARANG   [2]")
-	fmt.Println("%s\n", "CEK BARANG    [3]")
-	fmt.Println("=====================|  TOKO KEREN  |=====================")
+	fmt.Println("======================| ADMIN MENU |=======================")
+	fmt.Println("|                        [ MENU ]                         |")
+	fmt.Println("|                                                         |")
+	fmt.Println("|                    1. Input Barang                      |")
+	fmt.Println("|                    2. Edit Barang                       |")
+	fmt.Println("|                    3. Cek Barang                        |")
+	fmt.Println("|                    4. Exit                              |")
+	fmt.Println("|                                                         |")
+	fmt.Println("======================|  TOKO KEREN  |====================")
 }
 
 func isiBarang(b *arrBarang, n *int) {
 	var namaBarang, kategori string
 	var hargaModal, hargaJual float64
+	var transaksi int
 	fmt.Println("=====================| INPUT BARANG |=====================")
-	fmt.Println("Masukan dengan format")
-	fmt.Println("NAMA-BARANG KATAGORI HARGA-MODAL HARGA-JUAL")
+	fmt.Println("|                                                        |")
+	fmt.Println("|                MASUKAN DATA DENGAN FORMAT              |")
+	fmt.Println("| NAMA-BARANG KATAGORI HARGA-MODAL HARGA-JUAL TRANSAKSI  |")
+	fmt.Println("|                                                        |")
 	fmt.Println("=====================|  TOKO KEREN  |=====================")
-	fmt.Scan(&namaBarang, &kategori, &hargaModal, &hargaJual)
+	fmt.Scan(&namaBarang, &kategori, &hargaModal, &hargaJual, &transaksi)
 	for i := 0; i < NMAX && namaBarang != "XXX"; i++ {
 		b[i].nama = namaBarang
 		b[i].kategori = kategori
 		b[i].hargaModal = hargaModal
 		b[i].hargaJual = hargaJual
+		b[i].transaksi = transaksi
 		*n++
-		fmt.Scan(&namaBarang, &kategori, &hargaModal, &hargaJual)
+		fmt.Scan(&namaBarang, &kategori, &hargaModal, &hargaJual, &transaksi)
 	}
+}
+
+func editData(b *arrBarang, n, i int) {
+	fmt.Println("DENGAN FORMAT: NAMA-BARANG KATAGORI HARGA-MODAL HARGA-JUAL TRANSAKSI")
+	fmt.Scan(&b[i].nama, &b[i].kategori, &b[i].hargaModal, &b[i].hargaJual, &b[i].transaksi)
 }
 
 func cekBarang(b arrBarang, n int, target string) int {
